3571a4566717ae72c5f760c6e37355a7f1e551f3
Trícia Tapajós <mpasselewe@gmail.com>
2024-09-29 23:53:39 -0300

    mudando a port

--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,24 @@ import Fastify from "fastify";
 import { routes } from "./routes";
 import cors from '@fastify/cors';
 
-const app = Fastify({ logger: true })
+const app = Fastify({ logger: true });
 
 app.setErrorHandler((error, request, reply) => {
-    reply.code(400).send ({message: error.message})
-})
+    reply.code(400).send({ message: error.message });
+});
 
 const start = async () => {
-
     await app.register(cors);
     await app.register(routes);
 
     try {
-        await app.listen({ port: 3333 });
+        // Mude esta linha para escutar em 0.0.0.0
+        await app.listen({ port: 3333, host: '0.0.0.0' });
+        console.log('Servidor rodando na porta 0.0.0.0'); // Mensagem de log
     } catch (err) {
         console.error("Error starting server:", err);
         process.exit(1);
     }
-}
+};
+
 start();
